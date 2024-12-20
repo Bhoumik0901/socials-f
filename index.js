5330223964a16e3834df09f53f1966f1db50e71d
Bhoumik <Bhoumikchopra12@gmail.com>
2024-12-20 23:55:37 +0530

    socio u

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' })); // Set a body size limit if needed
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-// Routes
+// Route
 app.use(authRouter);
 app.use(commentRouter);
 app.use(postRouter);
